06a44a52dea7b7444c9f98b7b78ad22bf6ebfa56
Nicolai Austad <nicolaau@stud.ntnu.no>
2025-12-13 15:09:48 +0100

    removed prints

--- other_file.py
+++ other_file.py
@@ -105,9 +105,7 @@ def polygonal_cat(seed=None):
     for vertices, color in shapes:
         draw_polygon(pen, jitter(vertices), color)
     print("Just to make a new PR.")
-    print("Another line to make a new PR.")
-    print("Yet another line to make a new PR.")
-    print("Final line to make a new PR.")
+  
     turtle.done()
 
 
